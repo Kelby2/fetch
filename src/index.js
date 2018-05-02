14df2f9f28e078a45be7ffc18750ccc770614b7a
Kelby LU <kelby.lu28@gmail.com>
2018-05-02 12:33:55 -0400

    Add Promise return to ajax call

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import DOMNodeCollection from './DomNodeCollection';
+
+const fxArray = [];
+
+function $f(selector) {
+  let nodeArray;
+  switch (typeof selector) {
+    case 'string':
+      const nodeList = document.querySelectorAll(selector);
+      nodeArray = Array.from(nodeList);
+      break;
+    case 'function':
+      fxArray.push(selector);
+      break;
+    case HTMLElement:
+      nodeArray = Array.from(selector);
+      break;
+  }
+
+  return new DOMNodeCollection(nodeArray);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fxArray.forEach(fx => fx());
+});
+
+$f.extend = (...args) => {
+  return Object.assign({}, ...args);
+};
+
+_toQueryString = obj => {
+  let result;
+  for (const prop in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+      result += `${prop}=${obj[prop]&}`
+    }
+  }
+}
+
+$f.ajax = options => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    const defaults = {
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      method: "get",
+      url: "",
+      success: () => {},
+      error: () => {},
+      data: {},
+    }
+    const options = $f.extend(defaults, options);
+    options.method = options.method.toUpperCase();
+
+    xhr.open(options.method, options.url, true);
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject(JSON.parse(xhr.response));
+      }
+    }
+
+    xhr.send(JSON.stringify(options.data));
+  });
+};
